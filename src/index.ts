270cf962285aca494aa94efc446af16aab5191f0
Paul Marceau <paul@themarceaus.net>
2023-12-15 00:37:38 -0500

    Added index.ts file

---
+++ src/index.ts
@@ -0,0 +1,85 @@
+export {
+    IAuthPayload,
+    IAuth,
+    IAuthDocument,
+    IAuthBuyerMessageDetails,
+    IEmailMessageDetails,
+    ISignUpPayload,
+    ISignInPayload,
+    IForgotPassword,
+    IResetPassword,
+    IReduxAuthPayload,
+    IReduxAddAuthUser,
+    IReduxLogout,
+    IAuthResponse,
+    IAuthUser
+} from './interfaces/auth.interface';
+
+export {
+    IBuyerDocument,
+    IReduxBuyer
+} from './interfaces/buyer.interface';
+
+export {
+    IConversationDocument,
+    IMessageDocument,
+    IMessageDetails,
+    IChatBoxProps,
+    IChatSellerProps,
+    IChatBuyerProps,
+    IChatMessageProps
+} from './interfaces/chat.interface';
+
+export {
+    IEmailLocals
+} from './interfaces/email.interface';
+
+export {
+    GigType,
+    ICreateGig,
+    ISellerGig,
+    IGigContext,
+    IGigsProps,
+    IGigCardItems,
+    ISelectedBudget,
+    IGigViewReviewsProps,
+    IGigInfo,
+    IGigTopProps
+} from './interfaces/gig.interface';
+
+export {
+    IOffer,
+    IExtendedDelivery,
+    IDeliveredWork,
+    IOrderEvents,
+    IOrderReview,
+    IOrderMessage,
+    IOrderDocument,
+    IOrderNotifcation
+} from './interfaces/order.interface';
+
+export {
+    IReviewMessageDetails,
+    IRatingTypes,
+    IReviewDocument,
+    IRatingCategoryItem
+} from './interfaces/review.interface';
+
+export {
+    ISearchResult,
+    IHitsTotal,
+    IQueryList,
+    IQueryString,
+    ITerm,
+    IPaginateProps
+} from './interfaces/search.interface';
+
+export {
+    SellerType,
+    ILanguage,
+    IExperience,
+    IEducation,
+    ICertificate,
+    ISellerDocument
+} from './interfaces/seller.interface';
+
